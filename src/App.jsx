81da197b086dc10f2c001c528bfbaa9cef01658c
Sean <seanselbyy@gmail.com>
2024-12-03 16:49:58 +1300

    Dark mode works

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
+function Paragraph({ children }) {
+  return (
+    <p className="text-slate-500 dark:text-slate-400 mt-2 text-xl">
+      {children}
+    </p>
+  );
+}
 function App() {
 
   return (
-    <>
-      <h1 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Hello World</h1>
-      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <p>
-          This is a test page for tailwindcss!
-        </p>
+    <div>
+      <h1 className="text-slate-900 dark:text-white mt-5 text-3xl font-medium tracking-tight">
+        Hello World!
+      </h1>
+      <div className="bg-white dark:bg-slate-800 rounded-2xl px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <Paragraph>This is a test page for tailwindcss.</Paragraph>
       </div>
-      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-        I'm using tailwind css to  build a page that uses white-on-black for light mode and black-on-white for dark mode.
-      </p>
-    </>
-  )
+      <Paragraph>
+        I'm using tailwind css to build a page that uses white-on-black for
+        light mode and black-on-white for dark mode.
+      </Paragraph>
+    </div>
+  );
 }
 
-export default App
+export default App;
